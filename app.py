1a344cc796f28ae4f9338be7fc693a79374447e6
Luiz Alberto Manfroi <luiz.manfroi97@gmail.com>
2026-09-15 22:52:34 -0300

    Melhora indexacao e SEO do site

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, Response, render_template
 
 app = Flask(__name__)
 
@@ -6,6 +6,25 @@ app = Flask(__name__)
 @app.route("/")
 def inicio():
     return render_template("index.html")
+
+
+@app.route("/robots.txt")
+def robots():
+    return Response(
+        "User-agent: *\nAllow: /\n\nSitemap: https://oraculodode.com.br/sitemap.xml\n",
+        mimetype="text/plain",
+    )
+
+
+@app.route("/sitemap.xml")
+def sitemap():
+    return Response(
+        "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
+        "<urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\">"
+        "<url><loc>https://oraculodode.com.br/</loc></url>"
+        "</urlset>",
+        mimetype="application/xml",
+    )
 
 
 if __name__ == "__main__":
